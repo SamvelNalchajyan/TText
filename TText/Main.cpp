25947aa0a2df925f1180c4eb6ebe013a11584e17
Samvel Nalchajyan <nalchajyan.samvel@gmail.com>
2020-03-26 21:29:40 +0300

    Initial commit

---
+++ TText/Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <clocale>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	/*...........................................*/
+
+	/*...........................................*/
+	system("pause");
+	return 0;
+}
---
+++ Test/test_ttext.cpp
@@ -0,0 +1,1 @@
+#include "../TText/TText.h"
